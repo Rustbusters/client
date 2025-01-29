a7303ae9d7a7706c4d7eb4d33ceb6726b25ad092
Isax03 <isaia.tonini@gmail.com>
2025-01-29 09:20:28 +0100

    Fixed WebSocket Server

--- src/ui/websocket.rs
+++ src/ui/websocket.rs
@@ -7,29 +7,24 @@ const WEBSOCKET_PORT: u16 = HTTP_PORT + 1;
 
 pub(crate) fn run_websocket_server() {
     let listener = TcpListener::bind(format!("0.0.0.0:{WEBSOCKET_PORT}")).unwrap();
-    listener.set_nonblocking(true).ok();
-    loop {
-        match listener.accept() {
-            Ok((tcp_stream, _)) => {
-                println!("New WebSocket connection");
-                let web_socket_updates = thread::spawn(move || {
-                    if let Ok(web_socket_stream) = tungstenite::accept(tcp_stream) {
-                        handle_new_connection(web_socket_stream);
-                    }
-                });
-                THREADS.lock().unwrap().push(web_socket_updates);
-            }
-            Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
-                // No new connections, just continue
-            }
-            Err(err) => {
-                println!("Error: {}", err);
-            }
-        }
+    listener.set_nonblocking(true).unwrap();
 
-        let clients = CLIENTS_STATE.lock().unwrap();
-        if clients.is_empty() {
-            break;
+    for stream in listener.incoming() {
+        if let Ok(ws_stream) = stream {
+            println!("New Connection");
+            let web_socket_updates = thread::spawn(move || {
+                if let Ok(web_socket_stream) = tungstenite::accept(ws_stream) {
+                    handle_new_connection(web_socket_stream);
+                }
+            });
+            THREADS.lock().unwrap().push(web_socket_updates);
+
+            let clients = CLIENTS_STATE.lock().unwrap();
+            if clients.is_empty() {
+                break;
+            }
+        } else {
+            // println!("Error");
         }
     }
 
@@ -53,7 +48,7 @@ fn handle_new_connection(mut ws_stream: WebSocket<TcpStream>) {
                         msg.0,
                         serde_json::to_string(&msg.1).expect("Should be serializable")
                     );
-                    ws_stream.write(Message::Text(ws_message.into())).ok();
+                    ws_stream.send(Message::Text(ws_message.into())).ok();
                 }
             }
         }
