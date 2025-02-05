05000c4c28443614dc750ce43abbc7a0ee40781d
Isax03 <isaia.tonini@gmail.com>
2025-02-05 18:50:53 +0100

    fixed nack handler to the latest version

--- src/client/handlers/nack_handler.rs
+++ src/client/handlers/nack_handler.rs
@@ -2,7 +2,6 @@ use crate::client::RustbustersClient;
 use common_utils::{HostEvent, PacketHeader, PacketTypeHeader};
 use log::{info, warn};
 use wg_2024::network::SourceRoutingHeader;
-use wg_2024::packet::NackType::Dropped;
 use wg_2024::packet::{NackType, Packet};
 
 impl RustbustersClient {
@@ -25,97 +24,40 @@ impl RustbustersClient {
         nack_type: NackType,
         nack_header: &SourceRoutingHeader,
     ) {
-        match self
-            .pending_sent
-            .get(&(session_id, fragment_index))
-            .cloned()
-        {
+        match self.pending_sent.get(&(session_id, fragment_index)).cloned() {
             Some(mut packet) => {
-                if let Dropped = nack_type {
-                    info!("Client {}: Resending fragment {}", self.id, fragment_index);
-
-                    // Update stats for the dropping edge
-                    self.update_edge_stats_on_nack(&nack_header.hops);
-
-                    // Find a better path to reduce the probability of dropping the fragment
-                    self.reroute_packet(&mut packet, fragment_index, nack_header);
-
-                    // Resend the fragment
-                    if let Some(sender) = self.packet_send.get(&packet.routing_header.hops[1]) {
-                        if let Err(err) = sender.send(packet.clone()) {
-                            warn!(
-                                "Client {}: Unable to resend fragment {}: {}",
-                                self.id, fragment_index, err
-                            );
+                match nack_type {
+                    NackType::Dropped => {
+                        info!("Client {}: Resending fragment {}", self.id, fragment_index);
+                        self.update_edge_stats_on_nack(&nack_header.hops);
+                        
+                        // Check conditions for dropped packets
+                        let drop_from = nack_header.hops[0];
+                        let drop_to = nack_header.hops[1];
+                        let should_reroute = if let Some(stats) = self.edge_stats.get(&(drop_from, drop_to)) {
+                            stats.get_estimated_pdr() > 0.3 || stats.get_consecutive_nacks() >= 3
                         } else {
-                            self.send_to_sc(HostEvent::PacketSent(PacketHeader {
-                                session_id,
-                                pack_type: PacketTypeHeader::MsgFragment,
-                                routing_header: packet.routing_header.clone(),
-                            }));
-                        }
+                            false
+                        };
+                        
+                        self.reroute_and_resend(&mut packet, fragment_index, should_reroute);
                     }
-                } else {
-                    match nack_type {
-                        NackType::ErrorInRouting(drone) => {
-                            println!(
-                                "Client {}: Nack for fragment {} with type {:?}",
-                                self.id, fragment_index, nack_type
-                            );
-                            warn!(
-                                "Client {}: Nack for fragment {} with type {:?}",
-                                self.id, fragment_index, nack_type
-                            );
-                            self.topology.remove_node(drone);
-                            self.edge_stats
-                                .retain(|(from, to), _| *from != drone && *to != drone);
-                            self.known_nodes.lock().unwrap().remove(&drone);
-
-                            // Find a better path to reduce the probability of dropping the fragment
-                            self.reroute_packet(&mut packet, fragment_index, nack_header);
-
-                            if let Some(new_path) =
-                                self.find_weighted_path(*packet.routing_header.hops.last().unwrap())
-                            {
-                                // If there is a better path, reroute the packet
-                                if new_path != packet.routing_header.hops {
-                                    info!(
-                                        "Client {}: Rerouting packet for session {} fragment {} from path {:?} to {:?}",
-                                        self.id, packet.session_id, fragment_index, packet.routing_header.hops, new_path
-                                    );
-
-                                    // Update the packet's path
-                                    packet.routing_header.hops = new_path;
-                                    packet.routing_header.hop_index = 1;
-                                }
-                            }
-                            // Resend the fragment
-                            if let Some(sender) =
-                                self.packet_send.get(&packet.routing_header.hops[1])
-                            {
-                                if let Err(err) = sender.send(packet.clone()) {
-                                    warn!(
-                                        "Client {}: Unable to resend fragment {}: {}",
-                                        self.id, fragment_index, err
-                                    );
-                                } else {
-                                    self.send_to_sc(HostEvent::PacketSent(PacketHeader {
-                                        session_id,
-                                        pack_type: PacketTypeHeader::MsgFragment,
-                                        routing_header: packet.routing_header.clone(),
-                                    }));
-                                }
-                            }
-                        }
-                        NackType::DestinationIsDrone | NackType::UnexpectedRecipient(_) => {
-                            warn!(
-                                "Client {}: Nack for fragment {} with type {:?}",
-                                self.id, fragment_index, nack_type
-                            );
-                        }
-                        _ => {
-                            unreachable!("Unexpected nack type");
-                        }
+                    NackType::ErrorInRouting(drone) => {
+                        warn!(
+                            "Client {}: Nack for fragment {} with type {:?}",
+                            self.id, fragment_index, nack_type
+                        );
+                        self.topology.remove_node(drone);
+                        self.edge_stats
+                            .retain(|(from, to), _| *from != drone && *to != drone);
+                        self.known_nodes.lock().unwrap().remove(&drone);
+                        self.reroute_and_resend(&mut packet, fragment_index, true);
+                    }
+                    NackType::DestinationIsDrone | NackType::UnexpectedRecipient(_) => {
+                        warn!(
+                            "Client {}: Nack for fragment {} with type {:?}",
+                            self.id, fragment_index, nack_type
+                        );
                     }
                 }
             }
@@ -125,43 +67,52 @@ impl RustbustersClient {
         }
     }
 
-    /// Attempts to find a better route for a packet that was dropped.
+    /// Attempts to reroute and resend a packet after a failure.
     ///
-    /// This function is called when a packet needs to be rerouted due to
-    /// repeated failures or poor link quality. It uses edge statistics
-    /// to make routing decisions.
+    /// This function handles both dropped packets and routing errors by:
+    /// 1. Finding a new path to the destination
+    /// 2. Updating the packet's routing header
+    /// 3. Resending the packet through the new route
     ///
     /// ### Arguments
-    /// * `packet` - The packet to be rerouted
-    /// * `fragment_index` - The index of the fragment being rerouted
-    /// * `nack_header` - The source routing header of the NACK
-    fn reroute_packet(
+    /// * `packet` - The packet to be rerouted and resent
+    /// * `fragment_index` - The index of the fragment
+    /// * `force_reroute` - Whether to force rerouting regardless of conditions
+    fn reroute_and_resend(
         &mut self,
         packet: &mut Packet,
         fragment_index: u64,
-        nack_header: &SourceRoutingHeader,
+        force_reroute: bool,
     ) {
-        let drop_from = nack_header.hops[0];
-        let drop_to = nack_header.hops[1];
+        let destination = *packet.routing_header.hops.last().unwrap();
+        
+        if force_reroute {
+            if let Some(new_path) = self.find_weighted_path(destination) {
+                if new_path != packet.routing_header.hops {
+                    info!(
+                        "Client {}: Rerouting packet for session {} fragment {} from path {:?} to {:?}",
+                        self.id, packet.session_id, fragment_index, packet.routing_header.hops, new_path
+                    );
 
-        if let Some(stats) = self.edge_stats.get_mut(&(drop_from, drop_to)) {
-            // recompute path if the estimated PDR is above 0.3
-            if stats.get_estimated_pdr() > 0.3 || stats.get_consecutive_nacks() >= 3 {
-                if let Some(new_path) =
-                    self.find_weighted_path(*packet.routing_header.hops.last().unwrap())
-                {
-                    // If there is a better path, reroute the packet
-                    if new_path != packet.routing_header.hops {
-                        info!(
-                            "Client {}: Rerouting packet for session {} fragment {} from path {:?} to {:?}",
-                            self.id, packet.session_id, fragment_index, packet.routing_header.hops, new_path
-                        );
-
-                        // Update the packet's path
-                        packet.routing_header.hops = new_path;
-                        packet.routing_header.hop_index = 1;
-                    }
+                    packet.routing_header.hops = new_path;
+                    packet.routing_header.hop_index = 1;
                 }
+            }
+        }
+
+        // Attempt to resend the packet
+        if let Some(sender) = self.packet_send.get(&packet.routing_header.hops[1]) {
+            if let Err(err) = sender.send(packet.clone()) {
+                warn!(
+                    "Client {}: Unable to resend fragment {}: {}",
+                    self.id, fragment_index, err
+                );
+            } else {
+                self.send_to_sc(HostEvent::PacketSent(PacketHeader {
+                    session_id: packet.session_id,
+                    pack_type: PacketTypeHeader::MsgFragment,
+                    routing_header: packet.routing_header.clone(),
+                }));
             }
         }
     }
