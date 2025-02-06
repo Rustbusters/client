163bbb6f94a5887b6439efe927aadce42bc02a62
Isax03 <isaia.tonini@gmail.com>
2025-02-06 21:09:21 +0100

    Changed random message building

--- src/client/commands.rs
+++ src/client/commands.rs
@@ -1,7 +1,14 @@
 use crate::client::RustbustersClient;
-use common_utils::{ClientToServerMessage, HostCommand, HostMessage, ServerToClientMessage};
+use common_utils::{
+    ClientToServerMessage, HostCommand, HostMessage, MessageBody, MessageContent,
+    ServerToClientMessage,
+};
 use crossbeam_channel::Sender;
+use rand::distr::Alphanumeric;
+use rand::prelude::SliceRandom;
+use rand::{rng, Rng};
 use wg_2024::network::NodeId;
+use wg_2024::packet::NodeType;
 
 impl RustbustersClient {
     /// Handles various commands received from the controller
@@ -16,10 +23,34 @@ impl RustbustersClient {
     ) {
         match command {
             HostCommand::SendRandomMessage(dest) => {
+                let random_client_dest = {
+                    let nodes = self.known_nodes.lock().unwrap();
+                    let mut client_nodes: Vec<_> = nodes
+                        .iter()
+                        .filter(|(node_id, node_type)| {
+                            matches!(node_type, NodeType::Client) && **node_id != self.id
+                        })
+                        .collect();
+                    client_nodes.shuffle(&mut rng());
+                    client_nodes.first().map(|(node_id, _)| **node_id).unwrap()
+                };
+
                 self.send_message(
                     dest,
-                    HostMessage::FromClient(ClientToServerMessage::RegisterUser {
-                        name: "Random".to_string(),
+                    HostMessage::FromClient(ClientToServerMessage::SendPrivateMessage {
+                        recipient_id: random_client_dest,
+                        message: MessageBody {
+                            sender_id: self.id,
+                            timestamp: chrono::Local::now().format("%H:%M").to_string(),
+                            content: MessageContent::Text({
+                                let mut rng = rng();
+                                let length = rng.random_range(5..=20);
+                                let random_string: String = (0..length)
+                                    .map(|_| rng.sample(Alphanumeric) as char)
+                                    .collect();
+                                random_string
+                            }),
+                        },
                     }),
                     ws_to_ui_sender,
                 );
